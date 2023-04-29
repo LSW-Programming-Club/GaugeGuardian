8b0b5ca7e661115917eb7471081881cf0d01cb39
alex <aschwarz@xephyr.xyz>
2023-04-29 14:05:08 -0500

    functional map

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,35 @@ import express from 'express'
 
 import db from "../lib/db.js"
 
+import {parse} from 'csv-parse'
+import * as fs from 'fs';
+
 const router = express.Router()
 
 
 router.post('/', async (req, res) => {
   db.add()
   console.log(req.body)
+})
+
+router.get('/getAll', async (req, res) => {
+  //pipe data
+  var bridges = [];
+  var parser = await parse({columns: true}, function (err, records) {
+      records.forEach(function(record) {
+          if(record.longitude != '0' && bridges.length < 200) {
+              bridges.push({
+                  id: record.structureNumber,
+                  lat: parseInt(record.latitude) / 1000000,
+                  lng: parseInt(record.longitude) / 1000000,
+              });
+          }
+      });
+  });
+  fs.createReadStream('./nebraska.csv').pipe(parser).on("end", () => {
+      // Send the bridge data to the EJS template
+      res.status(200).json(bridges);
+  });
 })
 
 export { router }
--- routes/frontend.js
+++ routes/frontend.js
@@ -1,28 +1,10 @@
 import express from 'express'
-import {parse} from 'csv-parse'
-import * as fs from 'fs';
 
 var router = express.Router();
 
 //Map
 router.get("/", async function (req, res) {
-    //pipe data
-    var bridges = [];
-    var parser = await parse({columns: true}, function (err, records) {
-        records.forEach(function(record) {
-            if(record.longitude != '0') {
-                bridges.push({
-                    id: record.structureNumber,
-                    lat: parseInt(record.latitude) / 1000000,
-                    lng: parseInt(record.longitude) / 1000000,
-                });
-            }
-        });
-    });
-    fs.createReadStream('./nebraska.csv').pipe(parser).on("end", () => {
-        // Send the bridge data to the EJS template
-        res.render("index", { bridgeData: bridges });
-    });
+    res.render("index")
 })
 
 //Bridge id info

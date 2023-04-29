7ffdd4e557625f091d01c292e40e1a45f38346d6
alex <aschwarz@xephyr.xyz>
2023-04-29 11:44:12 -0500

    work

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ const sqlite3 = require('sqlite3').verbose();
 var logger = require('morgan');
 app.use(logger('dev'));
 
+// Allows server side rendering
+app.set('view engine', 'ejs')
+
+// Static Web Files
+app.use(express.static('public'))
+// Flatten icons to /public for device support reasons
+app.use(express.static('public/icons'))
+
+// Import routing from other routers in ./routes
+var frontend = require('./routes/frontend');
+var api = require('./routes/api');
+
+app.use("/", frontend);
+app.use("/api", api);
+
 //start the Express server
 app.listen(8080, () => {
     console.log(`server started at http://localhost:8080` );
